db6f90088bff5f99c0f25d4a25768f62804a8bf9
Joseph Keogh <jgk7uf@virginia.edu>
2019-07-18 15:00:09 -0400

    part of first try to run program on Rivanna

---
+++ PythonFiles/ODIdentification/SuperMain.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# variables moving forward
+printProgress=true
+testPerformance=false
+
+# the iteration we are on
+iteration=$1
+
+# analyze the data
+# the 0 at the end will become $iteration
+python Main.py trips utc2017AllDay.csv AllXDInfo-17.csv totalOutputTest $printProgress $testPerformance 0
+
+# view the output of the analysis
+# more totalOutputTest.txt
