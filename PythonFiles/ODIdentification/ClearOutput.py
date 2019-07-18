76b755e606cc7cd349d0002cb722de2f09d82f3a
Joseph Keogh <jgk7uf@virginia.edu>
2019-07-18 15:34:53 -0400

    changed clearoutput to make and clear separate output files for each run

--- PythonFiles/ODIdentification/ClearOutput.py
+++ PythonFiles/ODIdentification/ClearOutput.py
@@ -1,12 +1,28 @@
 import csv
 
-with open("totalOutput2017.txt", 'w') as file:
-    file.write("Total Trips: " + str(0))
-    file.write("\nInt Trips  : " + str(0))
-    file.write("\nOD Maps    : " + str(0))
-    file.write("\nTime Maps  : " + str(0))
-    file.write("\nPath Maps  : " + str(0))
-file.close()
+'''clear all the output files'''
+for i in range(0, 6):
+
+    i = str(i)
+
+    fileName = "totalOutput2017" + i + ".txt"
+
+    with open(fileName, 'w') as file:
+        file.write("Total Trips: " + str(0))
+        file.write("\nInt Trips  : " + str(0))
+        file.write("\nOD Maps    : " + str(0))
+        file.write("\nTime Maps  : " + str(0))
+        file.write("\nPath Maps  : " + str(0))
+    file.close()
+
+    fileName = "totalOutput2018" + i + ".txt"
+    with open(fileName, 'w') as file:
+        file.write("Total Trips: " + str(0))
+        file.write("\nInt Trips  : " + str(0))
+        file.write("\nOD Maps    : " + str(0))
+        file.write("\nTime Maps  : " + str(0))
+        file.write("\nPath Maps  : " + str(0))
+    file.close()
 
 with open("totalOutputTest.txt", 'w') as file:
     '''write the header'''
@@ -16,26 +32,6 @@ with open("totalOutputTest.txt", 'w') as file:
     file.write("\nTime Maps  : " + str(0))
     file.write("\nPath Maps  : " + str(0))
 file.close()
-
-with open("totalOutput2018.txt", 'w') as file:
-    file.write("Total Trips: " + str(0))
-    file.write("\nInt Trips  : " + str(0))
-    file.write("\nOD Maps    : " + str(0))
-    file.write("\nTime Maps  : " + str(0))
-    file.write("\nPath Maps  : " + str(0))
-file.close()
-
-with open("OutputTest.csv", 'w', newline='') as csvFile:
-    writer = csv.writer(csvFile)
-csvFile.close()
-
-with open("Output2017.csv", 'w', newline='') as csvFile:
-    writer = csv.writer(csvFile)
-csvFile.close()
-
-with open("Output2018.csv", 'w', newline='') as csvFile:
-    writer = csv.writer(csvFile)
-csvFile.close()
 
 print('Done Clearing Files')
 
