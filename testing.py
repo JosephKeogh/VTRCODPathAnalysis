5dd0c013bb4c7f0c51122e5b22473724e8b963e3
Joseph Keogh <jgk7uf@virginia.edu>
2019-07-24 13:19:54 -0400

    deleted som useless files

---
+++ testing.py
@@ -0,0 +1,18 @@
+import csv
+
+with open("smaller-050.csv", 'r', newline='') as f:
+
+    reader = csv.reader(f)
+
+    count = 0
+
+    for line in reader:
+
+        count += 1
+
+        l = line.__len__()
+
+        if l < 11:
+            print(count)
+            print(line)
+f.close()
