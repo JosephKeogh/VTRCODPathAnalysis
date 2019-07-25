9d7a8a17cc3e07253c8b93d270db5090d0a5b59c
Joseph Keogh <jgk7uf@virginia.edu>
2019-07-25 12:36:53 -0400

    print out sbatch outputs

--- PythonFiles/ODIdentification/start18.sh
+++ PythonFiles/ODIdentification/start18.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 module load anaconda
 sbatch --array=0-208 request18.slurm >> r1.txt
+echo r1.txt
 job1=`python getJobNum.py r1.txt`
 rm r1.txt
 sbatch --dependency=afterok:$job1 combine18.slurm >> r2.txt
+echo r2.txt
 job2=`python getJobNum.py r2.txt`
 rm r2.txt
 sbatch --dependency=afterok:$job2 analyze18.slurm
