7750684dff96044d11e06fe29328916c99a6880a
Joseph Keogh <jgk7uf@virginia.edu>
2019-07-25 11:33:51 -0400

    ` not single quote

--- PythonFiles/ODIdentification/start17.sh
+++ PythonFiles/ODIdentification/start17.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 module load anaconda
 sbatch --array=0-203 request17.slurm >> r1.txt
-job1='python getJobNum.py r1.txt'
+job1=`python getJobNum.py r1.txt`
 rm r1.txt
 sbatch --dependency=afterok:$job1 combine17.slurm >> r2.txt
-job2='python getJobNum.py r2.txt'
+job2=`python getJobNum.py r2.txt`
 rm r2.txt
 sbatch --dependency=afterok:$job2 analyze17.slurm
