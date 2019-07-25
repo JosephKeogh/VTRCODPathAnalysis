a3e29d1669eede35aef4f7d8c0d24ab9421c6754
Joseph Keogh <jgk7uf@virginia.edu>
2019-07-25 08:23:34 -0400

    18 not 17

--- PythonFiles/ODIdentification/start18.sh
+++ PythonFiles/ODIdentification/start18.sh
@@ -1,15 +1,15 @@
 module load anaconda
 
-sbatch --array=0-203 request17.slurm >> r1.txt
+sbatch --array=0-208 request18.slurm >> r1.txt
 
 job1='python getJobNum.py r1.txt'
 
 rm r1.txt
 
-sbatch --dependency=afterok:${job1} combine17.slurm >> r2.txt
+sbatch --dependency=afterok:${job1} combine18.slurm >> r2.txt
 
 job2='python getJobNum.py r2.txt'
 
 rm r2.txt
 
-sbatch --dependency=afterok:${job2} analyze17.slurm
+sbatch --dependency=afterok:${job2} analyze18.slurm
