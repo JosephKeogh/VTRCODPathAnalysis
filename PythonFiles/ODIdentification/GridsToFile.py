23b85cbad250600a69cf686fedf1eb7531931d16
Joseph Keogh <jgk7uf@virginia.edu>
2019-07-26 06:33:36 -0400

    renaming PathCounts output from analyzeoutput

--- PythonFiles/ODIdentification/GridsToFile.py
+++ PythonFiles/ODIdentification/GridsToFile.py
@@ -25,32 +25,6 @@ dcSize = 32.1869
 dcNum = int(dcSize / 4.82)
 dcGrid = TLDcCoor.createGrid(dcSize, dcNum)
 
-with open("ODNodes.txt", 'w') as file:
-    # writer = csv.writer(file)
-    # writer.writerow(["ID", "Type", "CenterLat", "CenterLong", "Radius"])
-    file.write("ID, Type, CenterLat, CenterLong\n")
-    # file.write("CenterLat, CenterLong" + "\n")
-
-    IDCount = 0
-
-    for o in novaGrid:
-        # writer.writerow([IDCount, "O", o.lat, o.long, originClose])
-        # writer.writerow([o.lat, o.long])
-        file.write(str(IDCount) + "," + "NOVA" + "," + str(o.lat) + "," + str(o.long) + "\n")
-        # file.write(str(o.lat) + "," + str(o.long) + "\n")
-
-        IDCount += 1
-
-    for d in dcGrid:
-        # writer.writerow([IDCount, "D", d.lat, d.long, destClose])
-        # writer.writerow([d.lat, d.long])
-        file.write(str(IDCount) + "," + "DC" + "," + str(d.lat) + "," + str(d.long) + "\n")
-        # file.write(str(d.lat) + "," + str(d.long) + "\n")
-
-        IDCount += 1
-
-file.close()
-
 with open("TableauInput.csv", 'w', newline='') as file:
     writer = csv.writer(file)
 
@@ -58,6 +32,9 @@ with open("TableauInput.csv", 'w', newline='') as file:
 
     for anode in dcGrid:
         writer.writerow([anode.getLat(), anode.getLong(), "DC"])
+
+    for bnode in novaGrid:
+        writer.writerow([bnode.getLat(), bnode.getLong(), "NOVA"])
 
 
 file.close()
